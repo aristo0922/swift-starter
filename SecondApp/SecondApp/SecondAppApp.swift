f0ba99ea890bf1dcded6cb7cda3f8cc7bd0778db
aristo <dkfud2121@naver.com>
2025-02-19 09:45:11 +0900

    feat: try 2

---
+++ SecondApp/SecondApp/SecondAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondAppApp.swift
+//  SecondApp
+//
+//  Created by 장아령 on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SecondAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
